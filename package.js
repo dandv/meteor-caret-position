0c757db80091f212eb109b4266f4e85e9502f6f8
Dan Dascalescu <ddascalescu+github@gmail.com>
2014-10-28 21:53:36 -0700

    Update to Meteor 0.9+

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
-'use strict';
-
 Package.describe({
-  summary: '(x, y) coordinates of the caret in a textarea or input type="text"'
+  name: 'dandv:caret-position',
+  summary: '(x, y) coordinates of the caret in a textarea or input type="text"',
+  version: '2.1.1',
+  git: 'https://github.com/dandv/meteor-caret-position.git'
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
   api.export('getCaretCoordinates', 'client');
-  api.add_files('component/index.js', 'client');
+  api.addFiles('component/index.js', 'client');
 });
